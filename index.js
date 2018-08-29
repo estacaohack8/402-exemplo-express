7d74b2159ecf302220be09f09000c7af52ea7a93
Bruno Pimentel <bpimentel@mastertech.tech>
2018-08-29 11:02:31 -0300

    Adicionar tratamento de dados de formulário

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const gatos = require('./data/gatos.json');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
+app.use(bodyParser.urlencoded());
 
 app.get('', (req, res) => {
     res.render('index');
+});
+
+app.post('', (req, res) => {
+    let string = `Nome: ${req.body.nome} \nEmail: ${req.body.email} \nMensagem: ${req.body.mensagem} \n`;
+
+    fs.writeFile('mensagem.txt', string, {flag: 'a'}, (err) => {
+        res.render('obrigado');
+    });
 });
 
 app.get('/gatos', (req, res) => {
