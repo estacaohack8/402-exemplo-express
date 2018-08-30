bb6ffb5afd87cb07e870b6154125dd5b667625fa
Bruno Pimentel <bpimentel@mastertech.tech>
2018-08-30 11:26:20 -0300

    Adicionar conexão com mongodb

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const expressMongoDb = require('express-mongo-db');
 const gatos = require('./data/gatos.json');
 
 const app = express();
 app.set('view engine', 'ejs');
 app.use('/static', express.static('static'));
 app.use(bodyParser.urlencoded());
+app.use(expressMongoDb('mongodb://localhost/rent-a-cat'));
 
 app.get('', (req, res) => {
     res.render('index');
 });
 
 app.post('', (req, res) => {
-    let string = `Nome: ${req.body.nome} \nEmail: ${req.body.email} \nMensagem: ${req.body.mensagem} \n`;
-
-    fs.writeFile('mensagem.txt', string, {flag: 'a'}, (err) => {
+    req.db.collection('mensagens').insert(req.body, (erro) => {
+        console.log(erro);
         res.render('obrigado');
     });
 });
