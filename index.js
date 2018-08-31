964eb102c1f23fdd7894f5bbc58ac766241d4a5d
Bruno Pimentel <bpimentel@mastertech.tech>
2018-08-31 09:32:30 -0300

    Deletar gato

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const expressMongoDb = require('express-mongo-db');
+const ObjectID = require('mongodb').ObjectID;
 
 const app = express();
 app.set('view engine', 'ejs');
@@ -42,13 +43,22 @@ app.get('/admin/gatos', (req, res) => {
 });
 
 app.get('/admin/gatos/inserir', (req, res) => {
-    res.render('admin-gatos-inserir');
+    res.render('admin-gatos-inserir', {'mensagem': ''});
 });
 
 app.post('/admin/gatos/inserir', (req, res) => {
     req.db.collection('gatos').insert(req.body, (erro) => {
         console.log(erro);
-        res.render('admin-gatos-inserir');
+        res.render('admin-gatos-inserir', {'mensagem': 'O gato foi inserido com sucesso.'});
+    });
+});
+
+app.post('/admin/gatos/:id', (req, res) => {
+    let id = ObjectID(req.params.id);
+
+    req.db.collection('gatos').remove({_id: id}, (erro) => {
+        console.log(erro);
+        res.redirect('/admin/gatos');
     });
 });
 
